1cca9f1acf016c7e3a675b4a0c39ac5c70c5fa93
SSRao <sindhusrao@gmail.com>
2019-02-12 16:58:08 +0100

    Edited overviewView

--- js/view/dishOverviewView.js
+++ js/view/dishOverviewView.js
@@ -6,33 +6,33 @@ var DishOverviewView = function (dishOverviewContainer, model) {
 	//needed buttons
 	this.editDinner = dishOverviewContainer.find("#editDinner");
 	this.printRecipe = dishOverviewContainer.find("#printRecipe");
-
+	var people = dishOverviewContainer.find("#people");
 	var showAllDishes = dishOverviewContainer.find("#showAllDishes");
 
 	var allDishes = model.getFullMenu();
 	var totalCost = model.getTotalMenuPrice();
 	var dishCost = 0;
 	var numberOfGuests = model.getNumberOfGuests();
-	
-	document.getElementById("showAllDishes").innerHTML += '<p> HELLLO I AM HERE KATA </p>'
 
 
 	var populateOverview = function(){
-		document.getElementById("people").innerHTML = numberOfGuests
-		document.getElementById("showAllDishes").innerHTML = '<div class="container">'+'<div class="row" style="margin-top:30px">'
+		people.empty();
+		people.append(numberOfGuests);
+		showAllDishes.empty();
+		showAllDishes.append('<div class="container">'+'<div class="row" style="margin-top:30px">');
 		allDishes.forEach(function(dish){
 
 			dishCost = model.getDishCost(dish);
 
-			document.getElementById("showAllDishes").innerHTML += '<div class="col-sm-3" style="padding-bottom:10px">'+
+			showAllDishes.append('<div class="col-sm-3" style="padding-bottom:10px">'+
 			'<div class="col-item">'+'<div class="photo">'+'<img src="'+dish.image+'" class="img-responsive" alt="a" />'+'</div>'+'<div class="info">'+'<div class="row">'+
 			'<div class="price col-md-12">'+'<h5>'+dish.title+'</h5>'+'<br/>'+'</div>'+
 			'</div>'+'<div class="separator clear-left">'+
 			'<i class="fa fa-list"></i><h5 class="price-text-color">'
-			+'</h5>'+'</div>'+'<div class="clearfix">'+'</div>'+'</div>'+'</div>'+'</div>'		
+			+'</h5>'+'</div>'+'<div class="clearfix">'+'</div>'+'</div>'+'</div>'+'</div>')		
 		});
-		document.getElementById("showAllDishes").innerHTML += '</div>'+'</div>'
-		document.getElementById("showAllDishes").innerHTML += '<div class="col-sm-12"><h3>Total Cost : '+totalCost+' SEK </h3></div>'
+		showAllDishes.append('</div>'+'</div>')
+		showAllDishes.append('<div class="col-sm-12"><h3>Total Cost : '+totalCost+' SEK </h3></div>')
 			
 	
 	}
